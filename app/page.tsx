b2498643701164d5215533e38a3fe7b742082173
Dell\Izza <izzajutt123@gmail.com>
2025-12-15 16:51:46 +0500

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-[400px]">
+     <section className="relative w-full
+      h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px]">
+      
       <Image
         src="/Surveys/main_banner.png"
         alt="Survey Banner"
         fill
-        className="object-cover"
         priority
+        className="object-cover"
+        sizes="
+          (max-width: 640px) 100vw,
+          (max-width: 1024px) 100vw,
+          100vw
+        "
       />
-    </div>
+    </section>
   );
 }
