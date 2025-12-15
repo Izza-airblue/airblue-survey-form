e1519c37f820a018d1a0b30c5c663b001606de00
Dell\Izza <izzajutt123@gmail.com>
2025-12-15 16:42:55 +0500

    .......

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <Image
-      src="/Surveys/main_banner.png"
-      alt="Survey Main Banner"
-      width={1200}
-      height={400}
-      priority
-    />
+      <div className="banner">
+      <Image
+        src="/Surveys/main_banner.png"
+        alt="Survey Banner"
+        fill
+        priority
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
   );
 }
