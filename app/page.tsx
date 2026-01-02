c859b1e29101ae9ec7b357bc6d320de7f8ac6a9c
Dell\Izza <izzajutt123@gmail.com>
2026-01-02 12:03:26 +0500

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -138,7 +138,7 @@ export default function Home() {
                 />
                 <div className="main_card_class absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
                 <div className="absolute bottom-4 left-4">
-                  <h2 className="text-white text-lg" style={{fontSize:"22px", fontWeight:"400"}}>
+                  <h2 className="text-white text-lg" style={{fontSize:"22px", fontWeight:"400",textAlign:"justify"}}>
                     {card.title}
                   </h2>
                   <p className="text-sm" style={{fontSize:"16px", fontWeight:"400",color:"white"}}>{card.subtitle}</p>
