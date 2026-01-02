f5aa3231c5ad61ca6762134d87508fd569bc90e5
Dell\Izza <izzajutt123@gmail.com>
2026-01-02 18:03:17 +0500

    ...

--- app/components/MealSurvey.tsx
+++ app/components/MealSurvey.tsx
@@ -15,6 +15,8 @@ interface Props {
 
 export const MealSurvey = ({ ratings, setRatings }: Props) => {
     const [mealType, setMealType] = useState("");
+    const [preferredMeal, setPreferredMeal] = useState("");
+    const [servedMeal, setServedMeal] = useState("");
 
     return (
         <>
@@ -39,58 +41,60 @@ export const MealSurvey = ({ ratings, setRatings }: Props) => {
             </div>
 
             <div className="mb-4">
-                <label className="form-label fw-semibold">
-                    5. Do you prefer a vegetarian or a non-vegetarian meal?
-                </label>
-                <div className="d-flex gap-4">
-                     {[
-                    { label: "Vegetarian", emoji: "🥬" },
-                    { label: "Non Vegetarian", emoji: "🍗" },
-                    ].map((item) => (
-                    <div
-                        key={item.label}
-                        onClick={() => setMealType(item.label)}
-                        className={`flex items-center gap-2 py-1.5 p-2 border rounded-md cursor-pointer text-sm transition-all
-                        ${
-                            mealType === item.label
-                            ? "border-2 border-primary bg-primary/10 shadow-sm"
-                            : "border-gray-300 text-gray-700 hover:border-primary"
-                        }
-                        `}
-                        style={{height:"40px", width:"150px"}}
-                    >
-                        <span className="text-base">{item.emoji}</span>
-                        <span className="font-medium">{item.label}</span>
-                    </div>
-                    ))}
+            <label className="form-label fw-semibold">
+                5. Do you prefer a vegetarian or a non-vegetarian meal?
+            </label>
+
+            <div className="d-flex gap-4">
+                {[
+                { label: "Vegetarian", emoji: "🥬" },
+                { label: "Non Vegetarian", emoji: "🍗" },
+                ].map((item) => (
+                <div
+                    key={item.label}
+                    onClick={() => setPreferredMeal(item.label)}
+                    className={`flex items-center gap-2 py-1.5 p-2 border rounded-md cursor-pointer transition-all
+                    ${
+                        preferredMeal === item.label
+                        ? "border-2 border-primary bg-primary/10 shadow-sm"
+                        : "border-gray-300 hover:border-primary"
+                    }
+                    `}
+                    style={{ height: "40px", width: "150px" }}
+                >
+                    <span className="text-base">{item.emoji}</span>
+                    <span className="font-medium">{item.label}</span>
                 </div>
+                ))}
+            </div>
             </div>
             <div className="mb-4">
-                <label className="form-label fw-semibold">
-                    6. Which meal was served to you on flight?
-                </label>
-                <div className="d-flex gap-4">
-                  {[
-                    { label: "Vegetarian", emoji: "🥬" },
-                    { label: "Non Vegetarian", emoji: "🍗" },
-                    ].map((item) => (
-                    <div
-                        key={item.label}
-                        onClick={() => setMealType(item.label)}
-                        className={`flex items-center gap-2 py-1.5 p-2 border rounded-md cursor-pointer text-sm transition-all
-                        ${
-                            mealType === item.label
-                            ? "border-2 border-primary bg-primary/10 shadow-sm"
-                            : "border-gray-300 text-gray-700 hover:border-primary"
-                        }
-                        `}
-                        style={{height:"40px", width:"150px"}}
-                    >
-                        <span className="text-base">{item.emoji}</span>
-                        <span className="font-medium">{item.label}</span>
-                    </div>
-                    ))}
+            <label className="form-label fw-semibold">
+                6. Which meal was served to you on flight?
+            </label>
+
+            <div className="d-flex gap-4">
+                {[
+                { label: "Vegetarian", emoji: "🥬" },
+                { label: "Non Vegetarian", emoji: "🍗" },
+                ].map((item) => (
+                <div
+                    key={item.label}
+                    onClick={() => setServedMeal(item.label)}
+                    className={`flex items-center gap-2 py-1.5 p-2 border rounded-md cursor-pointer transition-all
+                    ${
+                        servedMeal === item.label
+                        ? "border-2 border-primary bg-primary/10 shadow-sm"
+                        : "border-gray-300 hover:border-primary"
+                    }
+                    `}
+                    style={{ height: "40px", width: "150px" }}
+                >
+                    <span className="text-base">{item.emoji}</span>
+                    <span className="font-medium">{item.label}</span>
                 </div>
+                ))}
+            </div>
             </div>
             <div className="mb-4">
                 <label className="form-label fw-semibold">
