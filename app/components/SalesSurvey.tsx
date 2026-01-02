a6b6789e33ddcfda9b7116a045781b74f3430f33
Dell\Izza <izzajutt123@gmail.com>
2026-01-02 12:36:11 +0500

    ..

--- app/components/SalesSurvey.tsx
+++ app/components/SalesSurvey.tsx
@@ -2,13 +2,17 @@ export const SalesSurvey = () => {
     return (
         <>
             <select className="form-select mb-3">
-                <option>Please choose...</option>
+                <option>Please Choose...</option>
                 <option>Excellent</option>
                 <option>Good</option>
                 <option>Average</option>
                 <option>Poor</option>
             </select>
-            <textarea className="form-control" rows={3} />
+
+            <div className="mb-4">
+                <label className="form-label fw-semibold"> Remarks </label>
+                <textarea className="form-control" rows={4} placeholder="Please share any additional feedback or suggestions..." />
+            </div>
         </>
     )
 }
